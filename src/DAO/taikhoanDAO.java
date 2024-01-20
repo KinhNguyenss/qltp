2ca7ca5c72f95328ba157b216253f6c89edf37ac
KinhNguyen's <149393705+KinhNguyenss@users.noreply.github.com>
2024-01-20 13:28:15 +0700

    up

---
+++ src/DAO/taikhoanDAO.java
@@ -0,0 +1,26 @@
+package DAO;
+import connect.connectDb;
+import model.taikhoan;
+import java.sql.*;
+public class taikhoanDAO {
+      public static boolean checkLogin(taikhoan account) {
+
+        try {
+            String sql = "select * from taikhoan where Username=? and Password= ?";
+            Connection con = connectDb.getConnect();
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, account.getTaikhoan());
+            ps.setString(2, account.getMatkhau());
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return true;
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return false;
+    }
+}
---
+++ src/model/taikhoan.java
@@ -0,0 +1,36 @@
+package model;
+public class taikhoan {
+    private String taikhoan;
+    private String matkhau;
+
+    public taikhoan(String taikhoan, String matkhau) {
+        this.taikhoan = taikhoan;
+        this.matkhau = matkhau;
+    }
+
+    public String getTaikhoan() {
+        return taikhoan;
+    }
+
+    public void setTaikhoan(String taikhoan) {
+        this.taikhoan = taikhoan;
+    }
+
+    public String getMatkhau() {
+        return matkhau;
+    }
+
+    public void setMatkhau(String matkhau) {
+        this.matkhau = matkhau;
+    }
+
+    public taikhoan() {
+        super ();
+    }
+
+    @Override
+    public String toString() {
+        return "taikhoan{" + "taikhoan=" + taikhoan + ", matkhau=" + matkhau + '}';
+    }
+    
+}
